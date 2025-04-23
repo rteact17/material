fd7a1420436ce352dbe11f22db236bf9c209896a
rteact17 <reacttrainer17@gmail.com>
2025-04-23 21:27:52 +0530

    url corrected

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000'; // Update this with your backend URL
+// const API_URL = 'http://localhost:8000'; // Update this with your backend URL
+const API_URL = 'https://touchstoneserve.onrender.com'; 
 
 const api = axios.create({
   baseURL: API_URL,
